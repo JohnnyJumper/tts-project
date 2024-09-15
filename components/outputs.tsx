127a048ed568492b9ace642dae42c40d9d4f8018
Johnny Tahirov <jeyhunt@gmail.com>
2024-09-15 13:56:41 -0700

    basic sceleton for outputs component

--- components/outputs.tsx
+++ components/outputs.tsx
@@ -1,5 +1,14 @@
 import * as React from "react";
 
 export default function Outputs() {
-	return <div>Outputs box</div>
+  return (
+    <div className="max-w-[400px] w-full border border-slate-300 rounded-lg p-5">
+      <div className="mb-6">
+        <h1 className="font-semibold text-lg">Outputs</h1>
+        <sub className="text-sm text-slate-500">
+          This section will show your last 5 conversions.
+        </sub>
+      </div>
+    </div>
+  );
 }
