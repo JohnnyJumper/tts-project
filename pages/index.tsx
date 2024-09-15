90684fa837aa20c595cb60cd399763115e107c75
Johnny Tahirov <jeyhunt@gmail.com>
2024-09-15 12:05:16 -0700

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import * as React from "react";
 
 export default function Home() {
   return (
-    <div className="container my-8">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <div className="container my-12 flex justify-center flex-col items-center">
+      <h1 className="font-extrabold tracking-tight text-5xl">
         Kits AI Text-to-speech
       </h1>
+      <div className="max-w-[666px] pt-6">
+        <h4 className="text-xl text-center">
+          Play with unique AI voice models, languages, and pitch without the need for voice actors, microphones, or recordings.
+        </h4>
+      </div>
     </div>
   );
 }
