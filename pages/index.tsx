6025f6ac0d1d02eeea212a3c155541900415ae0b
Johnny Tahirov <jeyhunt@gmail.com>
2024-09-15 13:56:28 -0700

    mobile adaptation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,23 @@
 import Header from "@/components/header";
 import Outputs from "@/components/outputs";
 import TTS from "@/components/tts";
+import { cn } from "@/lib/utils";
 import * as React from "react";
 
 export default function Home() {
   return (
     <>
       <section>
-        <Header/>
+        <Header />
       </section>
-      <section className="flex w-full justify-between container">
+      <section
+        className={cn(
+          "flex container w-full flex-col items-center gap-5",
+          "lg:flex-row lg:justify-center lg:gap-8 lg:items-start"
+        )}
+      >
         <TTS />
-        <Outputs/>
+        <Outputs />
       </section>
     </>
   );
