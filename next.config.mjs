23fdae89b6c47d7dbe8a1754a9eeac2b69297802
Johnny Tahirov <jeyhunt@gmail.com>
2024-09-15 15:21:27 -0700

    add env for kits_voice_api

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    KITS_VOICE_API_KEY: process.env.KITS_VOICE_API_KEY
+  }
 };
 
 export default nextConfig;
