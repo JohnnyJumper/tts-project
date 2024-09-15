9f442a7efb80e83ae092ec85a48a9c0457bae865
Johnny Tahirov <jeyhunt@gmail.com>
2024-09-15 15:22:12 -0700

    attach voice model api to options

--- components/tts.tsx
+++ components/tts.tsx
@@ -2,8 +2,15 @@ import * as React from "react";
 import { Select, Field, Label, Textarea, Button } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { cn } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import { fetchVoiceModelInfo } from "@/pages/api/fetchVoiceModelInfo";
 
 export default function TTS() {
+  const { data: models, isLoading } = useQuery({
+    queryFn: async () => await fetchVoiceModelInfo(),
+    queryKey: ["voiceModelInfo"], //Array according to Documentation
+  });
+
   return (
     <div className="max-w-[400px] w-full border border-slate-300 rounded-lg p-5">
       <div className="mb-6">
@@ -18,16 +25,19 @@ export default function TTS() {
           Voice Model
         </Label>
         <Select
+          disabled={isLoading}
           className={cn(
             "border rounded-md w-full py-2 px-3 group appearance-none",
             "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
           )}
           data-focus
         >
-          <option value="active">Active</option>
-          <option value="paused">Paused</option>
-          <option value="delayed">Delayed</option>
-          <option value="canceled">Canceled</option>
+          {isLoading && <option>Loading...</option>}
+          {models?.data.map((model) => (
+            <option key={model.id} value={model.id}>
+              {model.title}
+            </option>
+          ))}
         </Select>
         <ChevronDownIcon
           className="group pointer-events-none absolute top-9 right-2.5 size-4 fill-slate-400"
--- pages/api/fetchVoiceModelInfo.ts
+++ pages/api/fetchVoiceModelInfo.ts
@@ -1,29 +1,6 @@
+import { PAGINATION_META, VOICE_MODEL_DATA } from "@/types/voiceAPI";
+
 const API_KEY = process.env.KITS_VOICE_API_KEY;
-
-type VOICE_MODEL_DATA = {
-  id: number;
-  title: string;
-  tags: string[] | null;
-  imageUrl: string | null;
-  demoURl: string | null;
-  twitterLink: string | null;
-  instagramLink: string | null;
-  tiktokLink: string | null;
-  spotifyLink: string | null;
-  youtubeLink: string | null;
-};
-
-type PAGINATION_META = {
-  total: number;
-  perPage: number;
-  currentPage: number;
-  lastPage: number;
-  firstPage: number;
-  firstPageUrl: string;
-  lastPageUrl: string;
-  nextPageUrl: string | null;
-  previousPageUrl: string | null;
-};
 
 type VOICE_API_RESPONSE = {
   data: VOICE_MODEL_DATA[];
@@ -40,5 +17,5 @@ export async function fetchVoiceModelInfo(): Promise<VOICE_API_RESPONSE> {
   };
   const url = "https://arpeggi.io/api/kits/v1/voice-models";
   const response = await fetch(url, options).then((r) => r.json());
-  return await response;
+  return response;
 }
