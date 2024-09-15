0c029412d572d30003538e8488a45484c0c8f99a
Johnny Tahirov <jeyhunt@gmail.com>
2024-09-15 16:05:59 -0700

    fix min height of outputs

--- components/outputs.tsx
+++ components/outputs.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export default function Outputs() {
   return (
-    <div className="max-w-[400px] w-full border border-slate-300 rounded-lg p-5">
+    <div className="max-w-[400px] w-full min-h-[368px] border border-slate-300 rounded-lg p-5">
       <div className="mb-6">
         <h1 className="font-semibold text-lg">Outputs</h1>
         <sub className="text-sm text-slate-500">
